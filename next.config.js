1f8fcc78d14c164062604e6b46b000834b001485
damaralbaribin <damaralbaribin@gmail.com>
2022-01-27 13:09:48 +0700

    feat : first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["cdn.gudangkomik.com","gudangkomik.com"]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,211 @@
 import '../styles/globals.css'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import type { AppProps } from 'next/app'
+import { Box, Container } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Box sx={{ backgroundColor: "#e5e7eb" }}>
+    <Component {...pageProps} />
+    <Container sx={{ backgroundColor: "white" }}>
+      <>
+        <h1>
+          <strong>
+            <span style={{ fontSize: 36 }}>
+              Situs Baca Manga Online Indonesia | Gudangkomik
+            </span>
+          </strong>
+        </h1>
+        <p>
+          <br />
+        </p>
+        <p>
+          <a href="https://gudangkomik.com/">
+            <strong>Gudangkomik</strong>
+          </a>{" "}
+          adalah situs <strong>Baca Manga Online Indonesia</strong> dengan koleksi
+          terlengkap dan terupdate. Kalian bisa membaca ratusan judul komik yang kami
+          update setiap hari secara gratis, dibalut dengan tampilan modern yang nyaman
+          dan responsif , website ini adalah tempat terbaik untuk kalian yang ingin{" "}
+          <strong>Baca Manga</strong> terbaru, Manhwa dan Manhua.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Kenapa Harus Baca Manga Online Di Gudangkomik?</strong>
+        </p>
+        <p>
+          Ada banyak sekali situs <strong>Manga Online</strong> yang bisa digunakan
+          untuk membaca komik yang kita inginkan dengan mudah di internet. Namun
+          karena alasan inilah kamu harus membacanya di situs{" "}
+          <strong>Gudangkomik</strong>:
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Baca Manga Online Secara Gratis</strong>
+        </p>
+        <p>
+          Di <strong>Gudangkomik</strong> kamu bisa baca beragam jenis manga secara
+          gratis tanpa harus mengeluarkan biaya langganan. Disini kamu bisa{" "}
+          <strong>Baca Manga</strong> apapun yang kamu suka dengan mudah secara gratis
+          selamanya.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Baca Manga Online Tanpa Iklan</strong>
+        </p>
+        <p>
+          Saat kamu membaca, iklan tentu akan sangat mengganggu sekali. Di situs{" "}
+          <strong>Gudangkomik</strong> kamu bisa membaca manga secara online tanpa
+          terganggu oleh adanya tayangan iklan. Kamu bisa membaca manga apapun dengan
+          mudah dan nyaman.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Tidak Loading</strong>
+        </p>
+        <p>
+          Selain itu, situs ini juga didukung oleh server handal yang bisa membuat
+          daya akses ke manga yang sangat cepat. Disini kamu bisa membaca manga yang
+          lebih seru dengan lebih cepat tanpa adanya loading yang membuat kamu
+          jengkel. Sebaliknya, kamu bisa membaca manga dengan kualitas HD pastinnya.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Dapat Diakses 24 Jam</strong>
+        </p>
+        <p>
+          Kapanpun kamu ingin membaca manga yang kamu mau, situs ini bisa kamu akses
+          dengan sangat mudah sekali selama 24 jam nonstop. Banyak pilihan manga yang
+          menarik, <strong>Gudangkomik</strong> menyediakan beragam jenis manga dalam
+          beragam Genre. Kamu bisa memilih jenis manga sesuai dengan yang kamu
+          inginkan dengan sangat mudah.
+        </p>
+        <p>
+          <br />
+        </p>
+        <h3>
+          <strong>
+            Ada Berbagai Macam Baca Manga Online di Gudangkomik, antara lain:
+          </strong>
+        </h3>
+        <h3>
+          <br />
+          <strong>Manhwa</strong>
+        </h3>
+        <p>
+          Manhwa adalah istilah Korea umum untuk komik dan kartun cetak{" "}
+          <a href="https://gudangkomik.com/" title="Baca Manga">
+            <strong>Baca Manga</strong>
+          </a>{" "}
+          Manhwa hanya di <strong>Gudangkomik</strong>, atau biasa disebut dengan
+          Komik Korea Di luar Korea, istilah tersebut biasanya merujuk pada komik
+          Korea Selatan, meskipun industri komik juga muncul di Korea Utara.Manhwa
+          adalah istilah Korea umum untuk komik dan kartun cetak. Di luar Korea,
+          istilah tersebut biasanya merujuk pada komik Korea Selatan, meskipun
+          industri komik juga muncul di Korea Utara.
+        </p>
+        <p>
+          <br />
+        </p>
+        <h3>
+          <strong>Manhua</strong>
+        </h3>
+        <p>
+          Manhua adalah komik Tiongkok yang diproduksi di Tiongkok dan di kawasan
+          Tiongkok Raya. Sedangkan komik Cina dan ilustrasi yang dinarasikan telah ada
+          di Cina dalam beberapa bentuk atau bentuk sepanjang sejarah kekaisarannya.
+        </p>
+        <p>
+          <br />
+        </p>
+        <h3>
+          <strong>Manga</strong>
+        </h3>
+        <p>
+          Manga adalah komik atau novel grafis yang berasal dari Jepang. Sebagian
+          besar manga sesuai dengan gaya yang dikembangkan di Jepang pada akhir abad
+          ke-19, dan bentuknya memiliki sejarah prasejarah yang panjang dalam seni
+          Jepang sebelumnya. Istilah manga digunakan di Jepang untuk merujuk pada
+          komik dan kartun.
+        </p>
+        <p>
+          <br />
+        </p>
+        <h3>
+          <strong>Apakah di Gudangkomik dapat membaca Manga Online lengkap?</strong>
+        </h3>
+        <p>
+          Tentu saja, semua manga yang sudah publish di publik akan tayang di situs
+          ini secara realtime. Bahkan ketika manganya masih tersedia Low Quality kamu
+          bisa{" "}
+          <a href="https://gudangkomik.com/" title="Baca Komik">
+            <strong>Baca Komik</strong>
+          </a>{" "}
+          tersebut di <strong>Gudangkomik</strong> secara gratis dan mudah. Bahkan
+          semua orang bisa membacanya dengan mudah menggunakan perangkat ponsel mereka
+          ataupun perangkat dekstop mereka kapanpun dan dimanapun. Mereka bisa akses
+          semaunya dengan gratis tanpa ada batasan waktu dan akses.
+        </p>
+        <p>
+          <span style={{ fontSize: 16 }}>
+            Daftar Genre Manga yang tersedia di situs <strong>Gudangkomik</strong>{" "}
+            menyediakan beragam jenis Genre Manga yang bisa kamu pilih sesuai dengan
+            keinginan kamu. Kamu bisa membaca beragam jenis manga terbaik yang paling
+            kamu suka.
+          </span>
+        </p>
+        <p>
+          <br />
+        </p>
+        <h4>
+          <strong>Tentang Baca Manga Online Gudangkomik</strong>
+        </h4>
+        <p>
+          <strong>Gudangkomik</strong> merupakan situs Baca{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Manga"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <strong>Manga</strong>
+          </a>{" "}
+          Online terlengkap yang berdiri sejak 2017. Didalamnya kalian dapat membaca
+          berbagai macam manga, manhwa, manhua dengan berbagai genre yang pastinya
+          sangat lengkap di Gudangkomik. Nikmati Baca Manga Online di website kami
+          setiap hari karena ada ratusan manga terupdate setiap harinya, di
+          Gudangkomik juga terdapat manga 18 yang dapat anda aktifkan di bagian
+          feature menu R18.
+        </p>
+        <p>
+          <br />
+        </p>
+        <h4>
+          <br />
+        </h4>
+        <p>
+          <br />
+        </p>
+      </>
+
+
+    </Container>
+  </Box >
 }
 
 export default MyApp
+
+
