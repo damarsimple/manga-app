8b3b93455c15683274da5eb640211843243f2d7a
damaralbaribin <damaralbaribin@gmail.com>
2022-02-01 17:22:59 +0700

    feat : no cache

--- modules/client.tsx
+++ modules/client.tsx
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, DefaultOptions } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
